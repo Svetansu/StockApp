c2af1e06b7e18848627e4fa2cdd3d77e6c117296
Svet <svetansusidhya@gmail.com>
2020-04-01 22:49:42 +0530

    stocks can be searched for

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
-
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 
+// bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
 //API key: pk_4fd99a1853cb4ad08ee4b64ada73457c
-function callAPI(finAPI) {
-	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_4fd99a1853cb4ad08ee4b64ada73457c', {json: true}, (err, res, body) => {
+function callAPI(finAPI, tick) {
+	request('https://cloud.iexapis.com/stable/stock/' + tick + '/quote?token=pk_4fd99a1853cb4ad08ee4b64ada73457c', {json: true}, (err, res, body) => {
 	if (err) {
 		return console.log(err); 
 	};
@@ -29,13 +34,24 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
-// Set HB Routes
+// Set HB index GET Routes
 app.get('/', function (req, res) {
     callAPI(function(doneAPI) {
     		res.render('home', {
     		info: doneAPI
     	}); 	
-    });
+    }, "amzn");
+});
+
+// Set HB index POST Routes
+// callAPI(function, req.body.stock_tick); -->what it is
+app.post('/', function (req, res) {
+    callAPI(function(doneAPI) {
+    		//postout = req.body.stock_tick;
+    		res.render('home', {
+    		info: doneAPI,
+    	}); 	
+    }, req.body.stock_tick);
 });
 
 app.get('/about.html', function (req, res) {
